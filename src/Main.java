34851efb301cad8809ecf753e8c02393fb398a70
Marcin Pucilowski <pucilowski.m@gmail.com>
2015-01-22 13:27:50 +0000

    trades/comms listeners

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,34 @@
  */
 public class Main {
 
-    public static void main(String[] args) {
+    public static final String HOST = "cs261.dcs.warwick.ac.uk";
+    public static final int TRADES_PORT = 80;
+    public static final int COMMS_PORT = 1720;
 
-        TcpListener l = new TcpListener("cs261.dcs.warwick.ac.uk", 80) {
+    TcpListener tradesIn;
+    TcpListener commsIn;
+
+    public Main() {
+        tradesIn = new TcpListener(HOST, TRADES_PORT) {
             @Override
             public void onLine(String in) {
-                System.out.println("In: " + in);
+                System.out.println("Trade: " + in);
             }
         };
 
-        new Thread(l).start();
+        commsIn = new TcpListener(HOST, COMMS_PORT) {
+            @Override
+            public void onLine(String in) {
+                System.out.println("Comm: " + in);
+            }
+        };
 
+        new Thread(tradesIn).start();
+        new Thread(commsIn).start();
+    }
 
+    public static void main(String[] args) {
+        new Main();
     }
 
 }
