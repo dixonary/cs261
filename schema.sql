0ec67fb80a9331403bcf924146dfbec4b30e70d7
Pucilowski <pucilowski.m@gmail.com>
2015-02-14 09:20:12 +0000

    MySQL uses # for comments

--- schema.sql
+++ schema.sql
@@ -1,10 +1,10 @@
---Suspicious Activity Detection Tool--
---Schema--
+#Suspicious Activity Detection Tool#
+#Schema#
 
 DROP TABLE IF EXISTS Trader CASCADE;
-CREATE TABLE Trader (						--TRADER TABLE--
-	email varchar(50) NOT NULL,				--Contains entities which represent individual Traders and averages for Trading volume and Profit
-	domain varchar(30) NOT NULL,			--There email addresses are unique and so are used as our primary key
+CREATE TABLE Trader (						#TRADER TABLE#
+	email varchar(50) NOT NULL,				#Contains entities which represent individual Traders and averages for Trading volume and Profit
+	domain varchar(30) NOT NULL,			#There email addresses are unique and so are used as our primary key
 	Tavg1 integer NOT NULL,
 	Tavg2 integer NOT NULL,
 	Tavg3 integer NOT NULL,
@@ -15,9 +15,9 @@ CREATE TABLE Trader (						--TRADER TABLE--
 );
 
 DROP TABLE IF EXISTS Symbol CASCADE;
-CREATE TABLE symbol (						--SYMBOL TABLE--
-	name varchar(10) NOT NULL,				--Contains entities which represent individual Stocks and averages for Trading volume and Profit
-	price float NOT NULL,					--Each Stock is unique on it's name/symbol
+CREATE TABLE symbol (						#SYMBOL TABLE#
+	name varchar(10) NOT NULL,				#Contains entities which represent individual Stocks and averages for Trading volume and Profit
+	price float NOT NULL,					#Each Stock is unique on it's name/symbol
 	totalTrades integer NOT NULL,
 	Tavg1 integer NOT NULL,
 	Tavg2 integer NOT NULL,
@@ -29,9 +29,9 @@ CREATE TABLE symbol (						--SYMBOL TABLE--
 );
 
 DROP TABLE IF EXISTS Sector CASCADE;
-CREATE TABLE sector (						--SECTOR TABLE--
-	name varchar(40) NOT NULL,				--Contains entities which represent individual sectors and averages for Trading volume
-	totalTrades integer NOT NULL,			--Each sector is unique on it's name
+CREATE TABLE sector (						#SECTOR TABLE#
+	name varchar(40) NOT NULL,				#Contains entities which represent individual sectors and averages for Trading volume
+	totalTrades integer NOT NULL,			#Each sector is unique on it's name
 	avg1 integer NOT NULL,
 	avg2 integer NOT NULL,
 	avg3 integer NOT NULL,
@@ -39,9 +39,9 @@ CREATE TABLE sector (						--SECTOR TABLE--
 );
 
 DROP TABLE IF EXISTS Trade CASCADE;
-CREATE TABLE Trade (						--TRADE TABLE--
-	id integer NOT NULL,					--Contains the full representation of a Trade as received from the Trades feed
-	time datetime NOT NULL,					--Also has an id as primary key as the timestamp cannot be guranteed to be unique
+CREATE TABLE Trade (						#TRADE TABLE#
+	id integer NOT NULL,					#Contains the full representation of a Trade as received from the Trades feed
+	time datetime NOT NULL,					#Also has an id as primary key as the timestamp cannot be guranteed to be unique
 	buyer varchar(50) NOT NULL,
 	seller varchar(50) NOT NULL,
 	price float NOT NULL,
@@ -59,10 +59,10 @@ CREATE TABLE Trade (						--TRADE TABLE--
 );
 
 DROP TABLE IF EXISTS CommLink CASCADE;
-CREATE TABLE CommLink (						--COMMLINK TABLE--
-	trader1 varchar(50) NOT NULL,			--Contains the link between individual Communications and individual Traders
-	trader2 varchar(50) NOT NULL,			--Tracks the history of communication between two traders in both directions and averages
-	totalTrades integer NOT NULL,			--Each entity is unique for each Trader pair
+CREATE TABLE CommLink (						#COMMLINK TABLE#
+	trader1 varchar(50) NOT NULL,			#Contains the link between individual Communications and individual Traders
+	trader2 varchar(50) NOT NULL,			#Tracks the history of communication between two traders in both directions and averages
+	totalTrades integer NOT NULL,			#Each entity is unique for each Trader pair
 	avg1 integer NOT NULL,
 	avg2 integer NOT NULL,
 	avg3 integer NOT NULL,
@@ -72,9 +72,9 @@ CREATE TABLE CommLink (						--COMMLINK TABLE--
 );
 	
 DROP TABLE IF EXISTS Communication CASCADE;
-CREATE TABLE Communication (				--COMMUNICATION TABLE--
-	id integer NOT NULL,					--Contains individual Communcations between Traders
-	time datetime NOT NULL,					--Has an id field as the timestamp cannot be guaranteed to be unique
+CREATE TABLE Communication (				#COMMUNICATION TABLE#
+	id integer NOT NULL,					#Contains individual Communcations between Traders
+	time datetime NOT NULL,					#Has an id field as the timestamp cannot be guaranteed to be unique
 	sender varchar(50) NOT NULL,
 	recipient varchar(50) NOT NULL,
 	PRIMARY KEY(id),
@@ -83,33 +83,33 @@ CREATE TABLE Communication (				--COMMUNICATION TABLE--
 );
 	
 DROP TABLE IF EXISTS Factor CASCADE;
-CREATE TABLE Factor (						--FACTOR TABLE--
-	factorId integer NOT NULL,				--Exists to provide a unique value for each individual factor to refer to
-	PRIMARY KEY(factorId)					--Each factorId is unique
+CREATE TABLE Factor (						#FACTOR TABLE#
+	factorId integer NOT NULL,				#Exists to provide a unique value for each individual factor to refer to
+	PRIMARY KEY(factorId)					#Each factorId is unique
 );
 	
 DROP TABLE IF EXISTS TradeFactor CASCADE;
-CREATE TABLE TradeFactor (					--TRADEFACTOR TABLE--
-	tradeId integer NOT NULL,				--Linking table between the Factor and Trade tables
-	factorId integer NOT NULL,				--Each entity is unique for a tradeId factorId pair
+CREATE TABLE TradeFactor (					#TRADEFACTOR TABLE#
+	tradeId integer NOT NULL,				#Linking table between the Factor and Trade tables
+	factorId integer NOT NULL,				#Each entity is unique for a tradeId factorId pair
 	PRIMARY KEY(tradeId,factorId),
 	FOREIGN KEY(tradeId)  REFERENCES Trade(id),
 	FOREIGN KEY(factorId) REFERENCES Factor(factorId)
 );
 
 DROP TABLE IF EXISTS CommFactor CASCADE;
-CREATE TABLE CommFactor (					--COMMFACTOR TABLE--
-	commId integer NOT NULL,				--Linking table between the Factor and Communication tables
-	factorId integer NOT NULL,				--Each entity is unique for a commId factorId pair
+CREATE TABLE CommFactor (					#COMMFACTOR TABLE#
+	commId integer NOT NULL,				#Linking table between the Factor and Communication tables
+	factorId integer NOT NULL,				#Each entity is unique for a commId factorId pair
 	PRIMARY KEY(commId,factorId),
 	FOREIGN KEY(commId)   REFERENCES Communication(id),
 	FOREIGN KEY(factorId) REFERENCES Factor(factorId)
 );
 
 DROP TABLE IF EXISTS EmailsFactor CASCADE;
-CREATE TABLE EmailsFactor (					--EMAILSFACTOR TABLE--
-	factorId integer NOT NULL,				--Represents the factor "Which traders are communicating with who"
-	email1 varchar(50) NOT NULL,			--Unique for each factorId
+CREATE TABLE EmailsFactor (					#EMAILSFACTOR TABLE#
+	factorId integer NOT NULL,				#Represents the factor "Which traders are communicating with who"
+	email1 varchar(50) NOT NULL,			#Unique for each factorId
 	email2 varchar(50) NOT NULL,
 	timeFrom datetime NOT NULL,
 	timeTo datetime NOT NULL,
@@ -118,9 +118,9 @@ CREATE TABLE EmailsFactor (					--EMAILSFACTOR TABLE--
 );
 
 DROP TABLE IF EXISTS PortfoliosFactor CASCADE;
-CREATE TABLE PortfoliosFactor (				--PORTFOLIOSFACTOR TABLE--
-	factorId integer NOT NULL,				--Represents the factor "Traders with similar portfolios"
-	email1 varchar(50) NOT NULL,			--Unique for each factorId
+CREATE TABLE PortfoliosFactor (				#PORTFOLIOSFACTOR TABLE#
+	factorId integer NOT NULL,				#Represents the factor "Traders with similar portfolios"
+	email1 varchar(50) NOT NULL,			#Unique for each factorId
 	email2 varchar(50) NOT NULL,
 	timeFrom datetime NOT NULL,
 	timeTo datetime NOT NULL,
@@ -129,9 +129,9 @@ CREATE TABLE PortfoliosFactor (				--PORTFOLIOSFACTOR TABLE--
 );
 
 DROP TABLE IF EXISTS StylesFactor CASCADE;
-CREATE TABLE StylesFactor (					--STYLESFACTOR TABLE--
-	factorId integer NOT NULL,				--Represents the factor "Traders with similar styles/trading patterns"
-	email1 varchar(50) NOT NULL,			--Unique for each factorId
+CREATE TABLE StylesFactor (					#STYLESFACTOR TABLE#
+	factorId integer NOT NULL,				#Represents the factor "Traders with similar styles/trading patterns"
+	email1 varchar(50) NOT NULL,			#Unique for each factorId
 	email2 varchar(50) NOT NULL,
 	timeFrom datetime NOT NULL,
 	timeTo datetime NOT NULL,
@@ -140,9 +140,9 @@ CREATE TABLE StylesFactor (					--STYLESFACTOR TABLE--
 );
 
 DROP TABLE IF EXISTS SectorsFactor CASCADE;
-CREATE TABLE SectorsFactor (				--SECTORSFACTOR TABLE--
-	factorId integer NOT NULL,				--Represents the factor "Active sectors"
-	sector varchar(40) NOT NULL,			--Unique for each factorId
+CREATE TABLE SectorsFactor (				#SECTORSFACTOR TABLE#
+	factorId integer NOT NULL,				#Represents the factor "Active sectors"
+	sector varchar(40) NOT NULL,			#Unique for each factorId
 	timeFrom datetime NOT NULL,
 	timeTo datetime NOT NULL,
 	PRIMARY KEY(factorId),
@@ -150,9 +150,9 @@ CREATE TABLE SectorsFactor (				--SECTORSFACTOR TABLE--
 );
 
 DROP TABLE IF EXISTS SymbolsFactor CASCADE;
-CREATE TABLE SymbolsFactor (				--SYMBOLSFACTOR TABLE--
-	factorId integer NOT NULL,				--Represents the factor "Active Stocks (symbol)"
-	symbol varchar(10) NOT NULL,			--Unique for each factorId
+CREATE TABLE SymbolsFactor (				#SYMBOLSFACTOR TABLE#
+	factorId integer NOT NULL,				#Represents the factor "Active Stocks (symbol)"
+	symbol varchar(10) NOT NULL,			#Unique for each factorId
 	timeFrom datetime NOT NULL,
 	timeTo datetime NOT NULL,
 	PRIMARY KEY(factorId),
@@ -160,9 +160,9 @@ CREATE TABLE SymbolsFactor (				--SYMBOLSFACTOR TABLE--
 );
 
 DROP TABLE IF EXISTS TradersFactor CASCADE;
-CREATE TABLE TradersFactor (				--TRADERSFACTOR TABLE--
-	factorId integer NOT NULL,				--Represents the factor "Particularly active Traders"
-	email varchar(50) NOT NULL,				--Unique for each factorId
+CREATE TABLE TradersFactor (				#TRADERSFACTOR TABLE#
+	factorId integer NOT NULL,				#Represents the factor "Particularly active Traders"
+	email varchar(50) NOT NULL,				#Unique for each factorId
 	timeFrom datetime NOT NULL,
 	timeTo datetime NOT NULL,
 	PRIMARY KEY(factorId),
@@ -170,9 +170,9 @@ CREATE TABLE TradersFactor (				--TRADERSFACTOR TABLE--
 );
 
 DROP TABLE IF EXISTS PerformancesFactor CASCADE;
-CREATE TABLE PerformancesFactor (			--PERFORMANCESFACTOR TABLE--
-	factorId integer NOT NULL,				--Represents the factor "Anomalous market performance for trader"
-	email varchar(50) NOT NULL,				--Unique for each factorId
+CREATE TABLE PerformancesFactor (			#PERFORMANCESFACTOR TABLE#
+	factorId integer NOT NULL,				#Represents the factor "Anomalous market performance for trader"
+	email varchar(50) NOT NULL,				#Unique for each factorId
 	timeFrom datetime NOT NULL,
 	timeTo datetime NOT NULL,
 	PRIMARY KEY(factorId),
@@ -180,9 +180,9 @@ CREATE TABLE PerformancesFactor (			--PERFORMANCESFACTOR TABLE--
 );
 
 DROP TABLE IF EXISTS PricesFactor CASCADE;
-CREATE TABLE PricesFactor (					--PRICESFACTOR TABLE--
-	factorId integer NOT NULL,				--Represents the factor "Sudden price changes of a stock"
-	email1 varchar(50) NOT NULL,			--Unique for each factorId
+CREATE TABLE PricesFactor (					#PRICESFACTOR TABLE#
+	factorId integer NOT NULL,				#Represents the factor "Sudden price changes of a stock"
+	email1 varchar(50) NOT NULL,			#Unique for each factorId
 	email2 varchar(50) NOT NULL,
 	symbol varchar(10) NOT NULL,
 	timeFrom datetime NOT NULL,
@@ -192,24 +192,24 @@ CREATE TABLE PricesFactor (					--PRICESFACTOR TABLE--
 );
 
 DROP TABLE IF EXISTS Cluster CASCADE;
-CREATE TABLE Cluster (						--CLUSTER TABLE--
-	clusterId integer NOT NULL,				--Links together a group of related clusters
-	PRIMARY KEY(clusterId)					--Each entity is unique for each clusterId
+CREATE TABLE Cluster (						#CLUSTER TABLE#
+	clusterId integer NOT NULL,				#Links together a group of related clusters
+	PRIMARY KEY(clusterId)					#Each entity is unique for each clusterId
 );
 	
 DROP TABLE IF EXISTS FactorCluster CASCADE;
-CREATE TABLE FactorCluster (				--FACTORCLUSTER TABLE--
-	clusterId integer NOT NULL,				--Linking table to associate multiple factors with one cluster
-	factorId integer NOT NULL,				--Entities are unique for each clusterId, factorId pair
+CREATE TABLE FactorCluster (				#FACTORCLUSTER TABLE#
+	clusterId integer NOT NULL,				#Linking table to associate multiple factors with one cluster
+	factorId integer NOT NULL,				#Entities are unique for each clusterId, factorId pair
 	PRIMARY KEY(clusterId,factorId),
 	FOREIGN KEY(clusterId) REFERENCES Cluster(clusterId),
 	FOREIGN KEY(factorId)  REFERENCES Factor(factorId)
 );
 	
 DROP TABLE IF EXISTS StockOwnership CASCADE;
-CREATE TABLE StockOwnership (				--STOCKOWNERSHIP TABLE--
-	symbol integer NOT NULL,				--Represents the volume of each stock that a Trader owns (can go negative!)
-	email varchar(50) NOT NULL,				--Each entity is unique for a symbol, email pair
+CREATE TABLE StockOwnership (				#STOCKOWNERSHIP TABLE#
+	symbol integer NOT NULL,				#Represents the volume of each stock that a Trader owns (can go negative!)
+	email varchar(50) NOT NULL,				#Each entity is unique for a symbol, email pair
 	volume integer NOT NULL,
 	lastUpdate datetime NOT NULL,
 	PRIMARY KEY(symbol,email),
