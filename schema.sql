0f6f5e67b83557dfc58ac3b94caa1fbabeff7e38
Marcin Pucilowski <pucilowski.m@gmail.com>
2015-02-19 11:26:32 +0000

    - return to convention (drop followed by create)

--- schema.sql
+++ schema.sql
@@ -3,48 +3,22 @@
 
 SET FOREIGN_KEY_CHECKS = 0;
 
-DROP TABLE IF EXISTS RawTrade CASCADE;
-DROP TABLE IF EXISTS RawComm CASCADE;
-DROP TABLE IF EXISTS Trader CASCADE;
-DROP TABLE IF EXISTS Symbol CASCADE;
-DROP TABLE IF EXISTS Sector CASCADE;
-DROP TABLE IF EXISTS Trade CASCADE;
-DROP TABLE IF EXISTS Comm CASCADE;
-DROP TABLE IF EXISTS CommLink CASCADE;
-DROP TABLE IF EXISTS StockOwnership CASCADE;
-
-DROP TABLE IF EXISTS TraderTrader CASCADE;
-DROP TABLE IF EXISTS ClusterFactor CASCADE;
-DROP TABLE IF EXISTS Factor CASCADE;
-DROP TABLE IF EXISTS Cluster CASCADE;
-
-DROP TABLE IF EXISTS TradeFactor CASCADE;
-DROP TABLE IF EXISTS CommFactor CASCADE;
-DROP TABLE IF EXISTS EmailsFactor CASCADE;
-DROP TABLE IF EXISTS PortfoliosFactor CASCADE;
-DROP TABLE IF EXISTS StylesFactor CASCADE;
-DROP TABLE IF EXISTS SectorsFactor CASCADE;
-DROP TABLE IF EXISTS SymbolsFactor CASCADE;
-DROP TABLE IF EXISTS TradersFactor CASCADE;
-DROP TABLE IF EXISTS PerformancesFactor CASCADE;
-DROP TABLE IF EXISTS PricesFactor CASCADE;
-
-SET FOREIGN_KEY_CHECKS = 1;
-
 # raw data tables
-
+DROP TABLE IF EXISTS RawTrade CASCADE;
 CREATE TABLE RawTrade (#RAWTRADE TABLE#
   id  INTEGER NOT NULL AUTO_INCREMENT, #Contains the full representation of a Trade as received from the Trades feed
   raw TEXT    NOT NULL,
   PRIMARY KEY (id)
 );
 
+DROP TABLE IF EXISTS RawComm CASCADE;
 CREATE TABLE RawComm (#RAWCOMM TABLE#
   id  INTEGER NOT NULL AUTO_INCREMENT, #Contains individual Communcations between Traders
   raw TEXT    NOT NULL,
   PRIMARY KEY (id)
 );
 
+DROP TABLE IF EXISTS Trader CASCADE;
 CREATE TABLE Trader (#TRADER TABLE#
   email       VARCHAR(50) NOT NULL, #Contains entities which represent individual Traders and averages for Trading volume and Profit
   domain      VARCHAR(30) NOT NULL, #There email addresses are unique and so are used as our primary key
@@ -58,6 +32,7 @@ CREATE TABLE Trader (#TRADER TABLE#
   PRIMARY KEY (email)
 );
 
+DROP TABLE IF EXISTS Symbol CASCADE;
 CREATE TABLE Symbol (#SYMBOL TABLE#
   name        VARCHAR(10) NOT NULL, #Contains entities which represent individual Stocks and averages for Trading volume and Profit
   price       FLOAT       NOT NULL DEFAULT 0, #Each Stock is unique on it's name/symbol
@@ -71,6 +46,7 @@ CREATE TABLE Symbol (#SYMBOL TABLE#
   PRIMARY KEY (name)
 );
 
+DROP TABLE IF EXISTS Sector CASCADE;
 CREATE TABLE Sector (#SECTOR TABLE#
   name        VARCHAR(40) NOT NULL, #Contains entities which represent individual sectors and averages for Trading volume
   totalTrades INTEGER     NOT NULL DEFAULT 0, #Each sector is unique on it's name
@@ -80,6 +56,7 @@ CREATE TABLE Sector (#SECTOR TABLE#
   PRIMARY KEY (name)
 );
 
+DROP TABLE IF EXISTS Trade CASCADE;
 CREATE TABLE Trade (#TRADE TABLE#
   id       INTEGER     NOT NULL AUTO_INCREMENT, #Contains the full representation of a Trade as received from the Trades feed
   time     LONG        NOT NULL, #Also has an id as primary key as the timestamp cannot be guranteed to be unique
@@ -99,6 +76,7 @@ CREATE TABLE Trade (#TRADE TABLE#
   FOREIGN KEY (sector) REFERENCES Sector (name)
 );
 
+DROP TABLE IF EXISTS Comm CASCADE;
 CREATE TABLE Comm (#COMM TABLE#
   id        INTEGER     NOT NULL AUTO_INCREMENT, #Contains individual Communcations between Traders
   time      LONG        NOT NULL, #Has an id field as the timestamp cannot be guaranteed to be unique
@@ -109,6 +87,7 @@ CREATE TABLE Comm (#COMM TABLE#
   FOREIGN KEY (recipient) REFERENCES Trader (email)
 );
 
+DROP TABLE IF EXISTS CommLink CASCADE;
 CREATE TABLE CommLink (#COMMLINK TABLE#
   trader1     VARCHAR(50) NOT NULL, #Contains the link between individual Communications and individual Traders
   trader2     VARCHAR(50) NOT NULL, #Tracks the history of communication between two traders in both directions and averages
@@ -121,6 +100,7 @@ CREATE TABLE CommLink (#COMMLINK TABLE#
   FOREIGN KEY (trader2) REFERENCES Trader (email)
 );
 
+DROP TABLE IF EXISTS StockOwnership CASCADE;
 CREATE TABLE StockOwnership (#STOCKOWNERSHIP TABLE#
   symbol     VARCHAR(10) NOT NULL, #Represents the volume of each stock that a Trader owns (can go negative!)
   email      VARCHAR(50) NOT NULL, #Each entity is unique for a symbol, Trader pair
@@ -133,6 +113,7 @@ CREATE TABLE StockOwnership (#STOCKOWNERSHIP TABLE#
 
 # analysis results
 
+DROP TABLE IF EXISTS TraderTrader CASCADE;
 CREATE TABLE TraderTrader (#TRADERTRADER TABLE#
   trader1     VARCHAR(50) NOT NULL, #Represents the number of Stocks a pair of Traders have in common
   trader2     VARCHAR(50) NOT NULL, #Each entity is unique for each Trader pair
@@ -145,6 +126,7 @@ CREATE TABLE TraderTrader (#TRADERTRADER TABLE#
   FOREIGN KEY (trader2) REFERENCES Trader (email)
 );
 
+DROP TABLE IF EXISTS Cluster CASCADE;
 CREATE TABLE Cluster (#CLUSTER TABLE#
   clusterId INTEGER NOT NULL AUTO_INCREMENT, #Links together a group of related factors
   time      LONG    NOT NULL,
@@ -152,6 +134,7 @@ CREATE TABLE Cluster (#CLUSTER TABLE#
   PRIMARY KEY (clusterId)          #Each entity is unique for each clusterId
 );
 
+DROP TABLE IF EXISTS Factor CASCADE;
 CREATE TABLE Factor (#FACTOR TABLE#
   factorId INTEGER NOT NULL AUTO_INCREMENT, #Exists to provide a unique value for each individual factor to refer to
   timeFrom LONG    NOT NULL,
@@ -159,6 +142,7 @@ CREATE TABLE Factor (#FACTOR TABLE#
   PRIMARY KEY (factorId)          #Each factorId is unique
 );
 
+DROP TABLE IF EXISTS ClusterFactor CASCADE;
 CREATE TABLE ClusterFactor (#FACTORCLUSTER TABLE#
   clusterId INTEGER NOT NULL, #Linking table to associate multiple factors with one cluster
   factorId  INTEGER NOT NULL, #Entities are unique for each clusterId, factorId pair
@@ -167,6 +151,7 @@ CREATE TABLE ClusterFactor (#FACTORCLUSTER TABLE#
   FOREIGN KEY (factorId) REFERENCES Factor (factorId)
 );
 
+DROP TABLE IF EXISTS TradeFactor CASCADE;
 CREATE TABLE TradeFactor (#TRADEFACTOR TABLE#
   tradeId  INTEGER NOT NULL, #Linking table between the Factor and Trade tables
   factorId INTEGER NOT NULL, #Each entity is unique for a tradeId factorId pair
@@ -175,6 +160,7 @@ CREATE TABLE TradeFactor (#TRADEFACTOR TABLE#
   FOREIGN KEY (factorId) REFERENCES Factor (factorId)
 );
 
+DROP TABLE IF EXISTS CommFactor CASCADE;
 CREATE TABLE CommFactor (#COMMFACTOR TABLE#
   commId   INTEGER NOT NULL, #Linking table between the Factor and Communication tables
   factorId INTEGER NOT NULL, #Each entity is unique for a commId factorId pair
@@ -183,6 +169,7 @@ CREATE TABLE CommFactor (#COMMFACTOR TABLE#
   FOREIGN KEY (factorId) REFERENCES Factor (factorId)
 );
 
+DROP TABLE IF EXISTS EmailsFactor CASCADE;
 CREATE TABLE EmailsFactor (#EMAILSFACTOR TABLE#
   factorId INTEGER     NOT NULL, #Represents the factor "Which traders are communicating with who"
   email1   VARCHAR(50) NOT NULL, #Unique for each factorId
@@ -191,6 +178,7 @@ CREATE TABLE EmailsFactor (#EMAILSFACTOR TABLE#
   FOREIGN KEY (factorId) REFERENCES Factor (factorId)
 );
 
+DROP TABLE IF EXISTS PortfoliosFactor CASCADE;
 CREATE TABLE PortfoliosFactor (#PORTFOLIOSFACTOR TABLE#
   factorId INTEGER     NOT NULL, #Represents the factor "Traders with similar portfolios"
   email1   VARCHAR(50) NOT NULL, #Unique for each factorId
@@ -199,6 +187,7 @@ CREATE TABLE PortfoliosFactor (#PORTFOLIOSFACTOR TABLE#
   FOREIGN KEY (factorId) REFERENCES Factor (factorId)
 );
 
+DROP TABLE IF EXISTS StylesFactor CASCADE;
 CREATE TABLE StylesFactor (#STYLESFACTOR TABLE#
   factorId INTEGER     NOT NULL, #Represents the factor "Traders with similar styles/trading patterns"
   email1   VARCHAR(50) NOT NULL, #Unique for each factorId
@@ -207,6 +196,7 @@ CREATE TABLE StylesFactor (#STYLESFACTOR TABLE#
   FOREIGN KEY (factorId) REFERENCES Factor (factorId)
 );
 
+DROP TABLE IF EXISTS SectorsFactor CASCADE;
 CREATE TABLE SectorsFactor (#SECTORSFACTOR TABLE#
   factorId INTEGER     NOT NULL, #Represents the factor "Active sectors"
   email1   VARCHAR(50) NOT NULL, #Unique for each factorId
@@ -216,6 +206,7 @@ CREATE TABLE SectorsFactor (#SECTORSFACTOR TABLE#
   FOREIGN KEY (factorId) REFERENCES Factor (factorId)
 );
 
+DROP TABLE IF EXISTS SymbolsFactor CASCADE;
 CREATE TABLE SymbolsFactor (#SYMBOLSFACTOR TABLE#
   factorId INTEGER     NOT NULL, #Represents the factor "Active Stocks (symbol)"
   email1   VARCHAR(50) NOT NULL, #Unique for each factorId
@@ -225,6 +216,7 @@ CREATE TABLE SymbolsFactor (#SYMBOLSFACTOR TABLE#
   FOREIGN KEY (factorId) REFERENCES Factor (factorId)
 );
 
+DROP TABLE IF EXISTS TradersFactor CASCADE;
 CREATE TABLE TradersFactor (#TRADERSFACTOR TABLE#
   factorId INTEGER     NOT NULL, #Represents the factor "Particularly active Traders"
   email    VARCHAR(50) NOT NULL, #Unique for each factorId
@@ -232,6 +224,7 @@ CREATE TABLE TradersFactor (#TRADERSFACTOR TABLE#
   FOREIGN KEY (factorId) REFERENCES Factor (factorId)
 );
 
+DROP TABLE IF EXISTS PerformancesFactor CASCADE;
 CREATE TABLE PerformancesFactor (#PERFORMANCESFACTOR TABLE#
   factorId INTEGER     NOT NULL, #Represents the factor "Anomalous market performance for trader"
   email    VARCHAR(50) NOT NULL, #Unique for each factorId
@@ -239,6 +232,7 @@ CREATE TABLE PerformancesFactor (#PERFORMANCESFACTOR TABLE#
   FOREIGN KEY (factorId) REFERENCES Factor (factorId)
 );
 
+DROP TABLE IF EXISTS PricesFactor CASCADE;
 CREATE TABLE PricesFactor (#PRICESFACTOR TABLE#
   factorId INTEGER     NOT NULL, #Represents the factor "Sudden price changes of a stock"
   email1   VARCHAR(50) NOT NULL, #Unique for each factorId
@@ -247,3 +241,6 @@ CREATE TABLE PricesFactor (#PRICESFACTOR TABLE#
   PRIMARY KEY (factorId),
   FOREIGN KEY (factorId) REFERENCES Factor (factorId)
 );
+
+
+SET FOREIGN_KEY_CHECKS = 1;
