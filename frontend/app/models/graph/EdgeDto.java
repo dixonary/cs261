c4d0eae99af0038ea90855176242130be524816b
Pucilowski <pucilowski.m@gmail.com>
2015-03-04 23:52:28 +0000

    -

---
+++ frontend/app/models/graph/EdgeDto.java
@@ -0,0 +1,19 @@
+package models.graph;
+
+/**
+ * Created by martin on 03/03/15.
+ */
+public class EdgeDto {
+
+    public int from;
+    public int to;
+
+    //public double value;
+
+    public String label;
+
+    public String title;
+
+    public EdgeDto() {
+    }
+}
---
+++ frontend/app/models/graph/NodeDto.java
@@ -0,0 +1,18 @@
+package models.graph;
+
+/**
+ * Created by martin on 03/03/15.
+ */
+public class NodeDto {
+
+    public int id;
+
+    public String label;
+
+    public String group;
+
+    public NodeDto() {
+    }
+
+
+}
