39519a97072a71f688394879431b4d091d5eb500
Pucilowski <pucilowski.m@gmail.com>
2015-03-04 23:39:34 +0000

    -

--- backend/src/main/java/team16/cs261/backend/module/impl/AnalyserModule.java
+++ backend/src/main/java/team16/cs261/backend/module/impl/AnalyserModule.java
@@ -54,8 +54,6 @@ public class AnalyserModule extends Module {
     @Autowired
     TraderStockDao traderStockDao;
 
-    @Autowired
-    GraphDao graphDao;
 
     @Autowired
     private Config config;
